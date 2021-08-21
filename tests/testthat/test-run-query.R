e399a63369b790436ed9fd41994441507d5bb889
Conor Neilson <condwanaland@gmail.com>
2021-08-21 13:50:09 +1200

    test run_query

---
+++ tests/testthat/test-run-query.R
@@ -0,0 +1,29 @@
+test_that("run_query tracks is expected shape", {
+  dat <- run_query(track_url(),
+                   params = list(
+    limit = 50,
+    offset = 0,
+    market = NULL))
+  
+  expect_equal(is.list(dat), TRUE)
+  expect_equal(is.data.frame(dat$items), TRUE)
+  expect_equal(nrow(dat$items), 50)
+  expect_equal(is.integer(dat$total), TRUE)
+  expect_equal(length(dat), 7)
+  expect_equal(unname(lengths(dat)), c(1, 30, 1, 1, 1, 0, 1))
+})
+
+test_that("run_query album is expected shape", {
+  dat <- run_query(album_url(),
+                   params = list(
+                     limit = 50,
+                     offset = 0,
+                     market = NULL))
+  
+  expect_equal(is.list(dat), TRUE)
+  expect_equal(is.data.frame(dat$items), TRUE)
+  expect_equal(nrow(dat$items), 50)
+  expect_equal(is.integer(dat$total), TRUE)
+  expect_equal(length(dat), 7)
+  expect_equal(unname(lengths(dat)), c(1, 26, 1, 1, 1, 0, 1))
+})
